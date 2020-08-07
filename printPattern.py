26149d71b50a03495a4efc4f91ba0c54a6f80c9c
Anujna P Rao <46913368+anujnaRao@users.noreply.github.com>
2020-08-07 16:32:25 +0530

    Specific Patteern printing

---
+++ printPattern.py
@@ -0,0 +1,11 @@
+
+if __name__ == '__main__':
+    n, m = map(int,input().split()) #This must be an odd number
+
+    for i in range(1,n,2):
+        print((i * ".|.").center(m, "-"))
+    print('WELCOME'.center(m, "-"))
+    
+    for i in range(n-2, -1, -2):
+        print((i * ".|.").center(m, "-"))
+
