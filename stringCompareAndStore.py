217f206e216de2b3986612c432ea2153e5d8cbe8
Anujna P Rao <46913368+anujnaRao@users.noreply.github.com>
2020-08-16 06:49:27 +0530

    Update stringCompareAndStore.py

--- stringCompareAndStore.py
+++ stringCompareAndStore.py
@@ -15,21 +15,52 @@ for user in args:
 
 print(output)
 
-# print("enter")
-# args = input().split(" ")
-# for i in args:
-#     li.append(i)
-# count = 1
-# output = []
-# output.append(li[0])
-# for i in range(len(li) + 1) :
-#     j = i + 1
-#     while j < len(li):
-#         if li[i] == li[j]:
-#             count = count + 1
-#             li[j] = li[j] + str(count)
-#         else:
-#             output.append(li[j])
-#             j += 1
-#             continue
-# print(output)
+#Another method of doing just using function
+
+output = []
+
+def checkuser(user):
+    if user not in output:
+        output.append(user)
+        # return user
+    else:
+        i = 1
+        temp =user
+        while temp in output:
+            temp = user+ str(i)
+            i += 1
+        output.append(temp)
+        # return temp
+
+args= input().split(" ")
+out = []
+for i in args:
+    # out.append(checkuser(i))
+    checkuser(i)
+# print(out)
+print(output)
+
+#Another method of doing using Class
+
+class Uses:
+    def __init__(self):
+        self.output = []
+    
+    def checkuser(self, user):
+        if user not in self.output:
+            self.output.append(user)
+            return user
+        else:
+            i = 1
+            temp = user
+            while temp in self.output:
+                temp = user + str(i)
+                i += 1
+            self.output.append(temp)
+            return temp
+
+if __name__ == "__main__":
+    s = input().split(" ")
+    ob = Uses()
+    s1 = list(map(ob.checkuser, s))
+    print(s1)
