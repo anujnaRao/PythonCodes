327f0db9d7ca9adf685e89cd340509b952780855
anujnaRao <anujna.prao5974@yahoo.com>
2020-09-22 23:31:52 +0530

    Testing code

--- Exceptions/test.py
+++ Exceptions/test.py
@@ -20,3 +20,38 @@ for m in range(0,n):
 val = max(a2)
 print(val)
 print(val * val)
+
+
+# #include <iostream>
+# #include <cmath>
+# #define MAX 10000
+# using namespace std;
+
+# int main(){
+
+#     int a[MAx], a2[MAX],n,t,val;
+#     cin >> t;
+#     for(int k=0;k<t;k++){
+#         cin >> n;
+#         for(int m = 0; m <n;m++){
+#             cin >> a[m];
+#         }
+
+#         for(int m = 0; m <n;m++){
+#             cout << a[m];
+#         }
+
+#         int i = 1, j = n -1;
+#         while( i < n ){
+#             a2[i-1] = a[j] - a[i];
+#         }
+#         for(int m = 0; m <n;m++){
+#             cout << a2[m];
+#         }
+        
+#         val = max(a2);
+#         cout << val;
+#         cout << (val * val);
+
+#     }
+# }
