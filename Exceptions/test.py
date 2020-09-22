aa03d7df64bf6f552806db908d3156befaf5728b
anujnaRao <anujna.prao5974@yahoo.com>
2020-09-22 23:29:56 +0530

    Testing code

---
+++ Exceptions/test.py
@@ -0,0 +1,22 @@
+a = [1, 2 , 3, 4, 5]
+a2 = []
+t = int(input("Enter "))
+for k in range (0,t):
+    n = int(input("Enter "))
+    for m in range(0,n):
+        a.append()
+    
+    for m in range(0,n):
+        print(a)
+
+i = 1
+j = 5 -1
+while i<5:
+    a2.append(a[j] - a[i] - 1)
+            
+for m in range(0,n):
+    print(a)
+        
+val = max(a2)
+print(val)
+print(val * val)
