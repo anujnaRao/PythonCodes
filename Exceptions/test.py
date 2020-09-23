18bbd2fcbc58e3bc9a5fbab64d05ff8ff7c2e6df
anujnaRao <anujna.prao5974@yahoo.com>
2020-09-23 22:53:26 +0530

    Test code

--- Exceptions/test.py
+++ Exceptions/test.py
@@ -1,25 +1,28 @@
-a = [1, 2 , 3, 4, 5]
+a = []
 a2 = []
 t = int(input("Enter "))
 for k in range (0,t):
+
     n = int(input("Enter "))
-    for m in range(0,n):
-        a.append()
     
     for m in range(0,n):
-        print(a)
-
-i = 1
-j = 5 -1
-while i<5:
-    a2.append(a[j] - a[i] - 1)
-            
-for m in range(0,n):
+        s = int(input("V: "))
+        a.append(s)
+    
     print(a)
-        
-val = max(a2)
-print(val)
-print(val * val)
+
+    i = 1
+    j = n -1
+    while i < n-1:
+        l = a[j] - a[i] - 1
+        a2.append(l)
+
+    i += 1           
+    print(a2)
+            
+    val = a2.sort().pop()
+    print(val)
+    print(val * val)
 
 
 # #include <iostream>
